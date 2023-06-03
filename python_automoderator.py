e91e5ebfa81f2b4bbb260c869670ec71d3e8b932
Habib ADOUM <habib.adoum01@gmail.com>
2023-06-03 21:22:08 +0200

    Add a parser for special characters and emojis

--- python_automoderator.py
+++ python_automoderator.py
@@ -1,5 +1,7 @@
 import requests
 import socket
+import unicodedata
+import sys
 
 # Twitch credentials
 client_id = 'CLIENT_ID'
@@ -34,7 +36,7 @@ def retrieve_twitch_chat(channel_name):
     port = 6667
 
     # Twitch IRC authentication credentials
-    nickname = 'justinfan123'
+    nickname = 'justinfan123' # Anonymous Twitch username
     token = get_oauth_token(client_id, client_secret)
 
     # Connect to the Twitch IRC server
@@ -46,16 +48,67 @@ def retrieve_twitch_chat(channel_name):
     irc.send(f'NICK {nickname}\n'.encode())
     irc.send(f'JOIN #{channel_name}\n'.encode())
 
+    count = 0 
     # Start receiving Twitch chat messages
     while True:
+        
+        count += 1
+        if count == 100: 
+            break
+
         data = irc.recv(2048).decode().strip()
         if data.startswith('PING'):
             irc.send('PONG :tmi.twitch.tv\n'.encode())
         elif len(data) > 0:
-            print(data)
+            # Parse the IRC message and extract the chat message
+            parsed_message = parse_chat_message(data)
+
+            if parsed_message is not None:
+                channel = parsed_message['channel']
+                username = parsed_message['username']
+                message = parsed_message['message']
+
+                # Normalize the username and message to handle special characters and emojis
+                normalized_username = unicodedata.normalize('NFKD', username)
+                normalized_message = unicodedata.normalize('NFKD', message)
+
+                # Print the channel name, username, and message with UTF-8 encoding
+                output = f'[#{channel}] {normalized_username}: {normalized_message}\n'
+                sys.stdout.buffer.write(output.encode('utf-8'))
+                sys.stdout.buffer.write(b'\n')
+                sys.stdout.flush()
 
     # Close the connection
     irc.close()
+
+def parse_chat_message(raw_message):
+    parts = raw_message.split(':', 2)
+    if len(parts) >= 3:
+        prefix = parts[1].strip()
+        channel = ''
+        username = ''
+        message = ''
+
+        # Extract channel name, username, and message based on different prefix formats
+        if 'PRIVMSG' in prefix:
+            channel = prefix.split('#', 1)[1].split(' ', 1)[0].strip()
+            username = prefix.split('!', 1)[0].strip()
+            message = parts[2].strip()
+        elif 'WHISPER' in prefix:
+            username = prefix.split('!', 1)[0].strip()
+            message = parts[2].strip()
+        elif 'USERNOTICE' in prefix:
+            channel = prefix.split('#', 1)[1].split(' ', 1)[0].strip()
+            message = parts[2].strip()
+
+        return {
+            'channel': channel,
+            'username': username,
+            'message': message
+        }
+
+    return None
+
 
 # Example usage
 retrieve_twitch_chat(channel)
